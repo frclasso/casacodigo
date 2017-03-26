0c961294e03c3b73feebddf37293c9061b8d5762
frclasso <frcalsso@yahoo.com.br>
2017-03-26 16:11:22 -0300

    first commit

---
+++ cap13-elementos-sintaxe-especifica/decorator2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+class trace_call:
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        print("Function executed: {} args: {}". format(self.f.__name__, args))
+
+        self.f(*args, **kwargs)
+
+
+@trace_call
+def add(a,b):
+    return a + b
+
+add(1, 3)
